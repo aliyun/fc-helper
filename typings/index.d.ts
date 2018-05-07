f1b5dca3172a17cb43666a40fbea42098e1eddbe
莫叁 <mosan.ly@alibaba-inc.com>
2018-05-07 11:14:21 +0800

    添加test yu asyncwrap类型

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,31 +1,77 @@
-interface FCRequest{
-    path:string;
-    method:string;
-    headers:any;
-    query:any;
+interface FCRequest {
+    path: string;
+    method: string;
+    headers: any;
+    query: any;
 }
 
-interface FCResponse{
-    statusCode:number;
-    headers:any;
-    body:any;
+interface FCResponse {
+    statusCode: number;
+    headers: any;
+    body: any;
 }
 
 interface Context {
-    req:FCRequest;
-    res:FCResponse;
-    path:string;
-    method:string;
-    type:String;
-    status:number;
-    body?:any;
-    set(key:string,value:string);
+    req: FCRequest;
+    res: FCResponse;
+    path: string;
+    method: string;
+    type: String;
+    status: number;
+    body?: any;
+    set(key: string, value: string);
 }
 
 interface FCMiddleWare {
-    (ctx: Context):Promise<any>;
+    (ctx: Context): Promise<any>;
+}
+
+interface FCHTTPEvent {
+
+}
+
+interface Credentials {
+    accessKeyId?: string;
+    accessKeySecret?: string;
+    securityToken?: string;
+}
+
+interface Functions {
+    name?: string;
+    handler?: string;
+    memory?: number;   // Integer, in MB.
+    timeout?: number;      // Integer, in seconds.
+}
+
+interface FCContext {
+    requestId?: string;
+    credentials?: Credentials;
+    "function"?: Functions;
+}
+
+interface FCRAWRequestEvent {
+    path: String;
+    httpMethod: String;
+    headers?: any;
+    queryParameters?: any
+    pathParameters?: any
+    body?: String;
+    isBase64Encoded?: boolean
+}
+
+interface FCRAWResponse {
+    isBase64Encoded?: boolean;
+    statusCode?: number;
+    headers?: any;
+    body?: string
+}
+
+interface Tester {
+    run(event: FCRAWRequestEvent, ctx: FCContext): Promise<FCRAWResponse>
 }
 
 declare module "fc-helper" {
     function hook(middleWare: FCMiddleWare)
+    function test(middleWare: FCMiddleWare): Tester
+    function asyncWrap(event: FCRAWRequestEvent, ctx: FCContext): any
 }
